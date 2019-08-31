48c02d24110ab719161d40055dcd9cfcdc562d82
Armen Mkrtchyan <armen@Armens-MacBook-Pro.local>
2019-08-31 13:10:05 -0400

    MOG-40: WIP Download endpoint

---
+++ src/Controller/Rest/Data/README.md
@@ -0,0 +1,81 @@
+# {repo_name}
+
+[![CI Status](http://img.shields.io/travis/{author_name}}/{repo_name}.svg?style=flat)](https://travis-ci.org/{author_name}/{repo_name})
+[![Version](https://img.shields.io/cocoapods/v/OpinionzRate.svg?style=flat)](http://cocoapods.org/pods/{repo_name})
+[![License](https://img.shields.io/cocoapods/l/{repo_name}.svg?style=flat)](http://cocoapods.org/pods/{repo_name})
+[![Platform](https://img.shields.io/cocoapods/p/{repo_name}.svg?style=flat)](http://cocoapods.org/pods/{repo_name})
+
+# Goal
+
+{goal}
+
+# Screenshots
+
+{screenshots}
+
+
+# Roadmap
+
+{roadmap}
+
+# Installation
+
+### {installation[0].title}
+
+{installation[0].description}
+
+### {installation[1].title}
+
+{installation[1].description}
+
+### {installation[2].title}
+
+{installation[2].description}
+
+# Usage
+
+<table>
+ <thead>
+<tr>
+<th>Example code</th>
+<th>Screenshot</th>
+</tr>
+</thead>
+<tr>
+<td>
+ {examples[0].title}
+  <pre lang="swift">
+  {examples[0].source_code}
+  </pre>
+</td>
+<td>
+ <img src="{examples[0].imageUrl}" alt="{examples[0].title}" width="250">
+</td>
+</tr>
+<tr>
+<td>
+{examples[1].title}
+<pre lang="swift">
+{examples[1].source_code}
+</pre>
+</td>
+<td>
+<img src="{examples[1].imageUrl}" alt="{examples[1].title}" width="250">
+</td>
+</tr>
+<tr>
+<td>
+{examples[2].title}
+<pre lang="swift">
+{examples[2].source_code}
+</pre>
+</td>
+<td>
+<img src="{examples[2].imageUrl}" alt="{examples[2].title}" width="250">
+</td>
+</tr>
+</table>
+
+# Contacts
+
+{contact}
--- src/Controller/Rest/MogoniController.php
+++ src/Controller/Rest/MogoniController.php
@@ -13,6 +13,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
+// Include the Response and ResponseHeaderBag
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
  * Class MogoniController
@@ -129,5 +133,156 @@ class MogoniController extends AbstractController
         $response->setContent($userContent);
 
         return $response;
+    }
+
+    /**
+     * This function is used to download product README.md
+     *
+     * @Route("/api/v1/download/product", methods={"POST"}, name="mogoni_download_product")
+     *
+     * @param Request $request
+     * @param ValidateService $validateService
+     *
+     * @return JsonResponse
+     * @throws
+     */
+    public function postDownloadProductAction(Request $request, ValidateService $validateService): Response
+    {
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // start DB transaction
+        $entityManager->getConnection()->beginTransaction();
+        $requestData = $request->request->all();
+
+        /** @var PublishProduct $product */
+        $product = $entityManager->getRepository(PublishProduct::class)->findOneBy(['repoName' => $requestData['repo_name']]);
+
+        // create new published product
+        if (!$product) {
+            $product = new PublishProduct();
+        }
+
+        /** @var Github $github */
+        $github = $entityManager->getRepository(Github::class)->find($requestData['id']);
+
+        $product->setGithub($github);
+        $product->setAuthorName($requestData['author_name'] ?? '');
+        $product->setRepoName($requestData['repo_name'] ?? '');
+        $product->setTitle($requestData['title'] ?? '');
+        $product->setDescription($requestData['description'] ?? '');
+        $product->setGoal($requestData['goal'] ?? '');
+        $product->setRoadmap($requestData['roadmap'] ?? '');
+        $product->setContact($requestData['contact'] ?? '');
+        $product->setScreenshots($requestData['screenshots'] ?? array());
+        $product->setExamples($requestData['examples'] ?? array());
+        $product->setInstallations($requestData['installations'] ?? array());
+
+
+        // The dinamically created content of the file
+        $content = $this->getReadmeTemplate();
+        $fileContent = $this->replacePlaceholders($content, $product);
+        
+        // Return a response with a specific content
+        $response = new Response($fileContent);
+
+        // Provide a name for your file with extension
+        $filename = 'README.md';
+
+        // Create the disposition of the file
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $filename
+        );
+
+        // Set the content disposition
+        $response->headers->set('Content-Disposition', $disposition);
+
+        // Dispatch request
+        return $response;
+    }
+
+    private function getReadmeTemplate() 
+    {
+        $finder = new Finder();
+        $finder->in(__DIR__);
+
+        $finder->path('Data/README.md');
+        foreach ($finder as $file) {
+            // print_r($file);
+            $contents = $file->getContents();
+            return $contents;
+        }
+    }
+
+    private function replacePlaceholders($content, $product) 
+    {
+        $content = str_replace('{author_name}', $product->getAuthorName(), $content);
+        $content = str_replace('{repo_name}', $product->getRepoName(), $content);
+        $content = str_replace('{goal}', $product->getGoal(), $content);
+        $content = str_replace('{roadmap}', $product->getRoadmap(), $content);
+        $content = str_replace('{contact}', $product->getContact(), $content);
+
+        $screenshotsTable = $this->getScreenshotsTable($product->getScreenshots());
+        $content = str_replace('{screenshots}', $screenshotsTable, $content);
+        // TODO: Installation blocks
+        // TODO: Examples code blocks
+        return $content;
+    }
+
+    private function getScreenshotsTable($screenshotsData) {
+        $screenshots = $this->getScreenshots($screenshotsData);
+        $titles = $this->getScreenshotsTitles($screenshotsData);
+
+        $array = array();
+        foreach ($screenshotsData as $value) {
+            $array[] = "--";
+        }
+        $separated = implode(" | ", $array);
+        $separated = "|" . $separated . "|";
+        
+        $content = <<<CONTENT
+$screenshots
+$separated
+$titles
+CONTENT;
+        return $content;
+    }
+
+    private function getScreenshots($screenshotsData) 
+    {
+        $templateScreenshot = "![{title}]({imageUrl})";
+
+        $array = array();
+        foreach ($screenshotsData as $screenshot) {
+            $title = $screenshot['title'];
+            $imageUrl = $screenshot['imageUrl'];
+            $newStr = str_replace('{title}', $title, $templateScreenshot);
+            $newStr = str_replace('{imageUrl}', $imageUrl, $newStr);
+            $array[] = $newStr;
+        }
+        $separated = implode(" | ", $array);
+        $separated = "| " . $separated . " |";
+
+        return $separated;
+    }
+
+    private function getScreenshotsTitles($screenshotsData)
+    {
+        $titles = "";
+
+        $templateTitle = "{title}";
+
+        $array = array();
+        foreach ($screenshotsData as $screenshot) {
+            $title = $screenshot['title'];
+            $imageUrl = $screenshot['imageUrl'];
+            $newStr = str_replace('{title}', $title, $templateTitle);
+            $array[] = $newStr;
+        }
+        $separated = implode(" | ", $array);
+        $separated = "| " . $separated . " |";
+
+        return $separated;
     }
 }
